c954428429e3d7969cf72ebf1b5330eed57a899b
Brandon Carlino <bacarlino@gmail.com>
2017-06-23 21:08:51 -0400

    Remove 'static' from STATIC_DIRS

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the prmysite/wsgi.mysite/wsgi.pymysite/wsgi.py like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -127,13 +128,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'assets')
+    os.path.join(PROJECT_ROOT, 'assets')
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 WEBPACK_LOADER = {
     'DEFAULT': {
