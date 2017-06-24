c0da38d1a41b843809d3c6a22ebb966f60b051a5
Brandon Carlino <bacarlino@gmail.com>
2017-06-23 23:21:13 -0400

    Add heroku url to allowed_hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'development_key')
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = [
-    '0.0.0.0'
+    '0.0.0.0',
+    'brandoncarlino.herokuapp.com'
 ]
 
 
