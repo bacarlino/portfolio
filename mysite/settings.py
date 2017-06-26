b0d2ae7b4365e3f9d3c2f823045ccae30a8007ba
Brandon Carlino <bacarlino@gmail.com>
2017-06-26 16:49:13 -0400

    Changed all simple views to generic 'TemplateView' and removed redundant views

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio.apps.PortfolioConfig',
+    'portfolio',
     'webpack_loader',
     'whitenoise.runserver_nostatic',
 ]
